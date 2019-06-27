184f03131931804949009ae852234c26689c83ca
Liam Wynn <wynnliam@pdx.edu>
2019-06-27 10:56:09 -0700

    Added simple function to print command line arguments

--- src/asciilines.hs
+++ src/asciilines.hs
@@ -1,1 +1,7 @@
-main = putStrLn "Hello"
+import System.Environment
+
+printArgs :: (Show a) => [a] -> IO ()
+printArgs [] = putStrLn ""
+printArgs x = putStrLn (show x)
+
+main = getArgs >>= printArgs
