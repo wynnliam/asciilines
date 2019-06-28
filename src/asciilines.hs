6151b1f0e4ac3cbcaf2aa1d01d6d8c906614f2f5
Liam Wynn <wynnliam@pdx.edu>
2019-06-28 13:04:48 -0700

    Added function verifyFileName.
    Returns Nothing if file name doesn't include .tvg. Otherwise
    it returns the file name (wrapped around a Just). This way
    we can know if the file name is actually a .tvg file.

--- src/asciilines.hs
+++ src/asciilines.hs
@@ -1,9 +1,10 @@
 import System.Environment
 import System.IO
 import System.Exit
+import Data.String
 
 -- For now, we shall only handle the case of empty argument list.
-main = parseArgs []
+main = getArgs >>= parseArgs
 
 -- Given our collection of arguments, we return some kind of IO action.
 -- We do not specify what the IO action is exactly, because it is done
@@ -14,7 +15,19 @@ parseArgs :: [String] -> IO a
 -- similar to >>= except that >> ignores the input. In this case, since
 -- exitSuccess is independent of printUsage, we ignore printUsage's result (which
 -- is () because printUsage prints to standard IO and quits).
-parseArgs [] = printUsage >> exitSuccess
+parseArgs []  = printUsage >> exitSuccess
+parseArgs [file] = putStrLn (show (verifyFileName file)) >> exitSuccess
 -- TODO: Handle case of non-empty argument list
 
 printUsage = putStrLn "No files given. Please run as ./asciilines file.tvg"
+
+-- Returns Just fileName if the file has a .tvg extension.
+-- Otherwise will return Nothing.
+verifyFileName :: String -> Maybe String
+verifyFileName file
+    | (length file) < 4 = Nothing
+    | (drop ((length file) - 3) file) == "tvg" = Just file
+    | otherwise = Nothing
+    -- Name has to be at least ".tvg". If not then return Nothing.
+    -- If the last three characters of the string are tvg, then we can read this.
+    -- Otherwise we return nothing.
