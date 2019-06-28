2996e0d022438f07ec28a36ba9ff368cd455afac
Liam Wynn <wynnliam@pdx.edu>
2019-06-28 11:35:45 -0700

    Updated program to handle empty argument list.
    When the user runs the application, they may
    supply no arguments. For this case, the program
    will display a message on application usage.

--- src/asciilines.hs
+++ src/asciilines.hs
@@ -1,7 +1,20 @@
 import System.Environment
+import System.IO
+import System.Exit
 
-printArgs :: (Show a) => [a] -> IO ()
-printArgs [] = putStrLn ""
-printArgs x = putStrLn (show x)
+-- For now, we shall only handle the case of empty argument list.
+main = parseArgs []
 
-main = getArgs >>= printArgs
+-- Given our collection of arguments, we return some kind of IO action.
+-- We do not specify what the IO action is exactly, because it is done
+-- on a case-by-case basis.
+parseArgs :: [String] -> IO a
+-- In this case, the application will simply print how to run the application
+-- correctly, then proceed to terminate without error. The >> operation is
+-- similar to >>= except that >> ignores the input. In this case, since
+-- exitSuccess is independent of printUsage, we ignore printUsage's result (which
+-- is () because printUsage prints to standard IO and quits).
+parseArgs [] = printUsage >> exitSuccess
+-- TODO: Handle case of non-empty argument list
+
+printUsage = putStrLn "No files given. Please run as ./asciilines file.tvg"
